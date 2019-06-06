7f63ea40b88524c475754677320525f1b053faf6
Nelev <smacchia.mattia@gmail.com>
2019-06-06 23:44:08 +0200

    feat: header menu toggle

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,25 @@ import { Button, Icon, Modal } from "antd";
 import "./style.less";
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
     const [showProfile, setShowProfile] = useState(false);
+
+    const Menu = () => {
+        return showMenu ? <div style={{ color: "black" }}>Menu</div> : null;
+    };
+
+    const MenuToogle = () => {
+        return (
+            <span
+                className="c-Header-MenuToggle"
+                onClick={() => {
+                    setShowMenu(!showMenu);
+                }}
+            >
+                <Icon type={showMenu ? "close" : "profile"} />
+            </span>
+        );
+    };
 
     const Profile = () => {
         return showProfile ? (
@@ -24,6 +42,7 @@ const Header = () => {
 
     return (
         <div className="c-Header">
+            <MenuToogle />
             <span className="c-HeaderTitle">
                 <FormattedMessage id="app.title" />
             </span>
@@ -38,6 +57,7 @@ const Header = () => {
                 </Button>
             </span>
             <Profile />
+            <Menu />
         </div>
     );
 };
